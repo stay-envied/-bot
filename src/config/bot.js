415f8ab306d6baefff085d132c4c8803af116bc4
stay-envied <mrspillit25@gmail.com>
2026-07-14 21:56:09 -0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,8 +23,8 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
-        state: "stalking",     // this is what people actually see
+        name: "butterflys", // required by Discord API, not shown in the client
+        state: "/butterflys",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
@@ -93,8 +93,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699",
-      secondary: "#2F3136",
+      primary: "#222124",
+      secondary: "#505051",
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287",
